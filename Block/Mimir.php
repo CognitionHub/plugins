f3bac885570c9f34e6cb11336baa815fdbbd2ea2
Jens <jensakrr@gmail.com>
2023-09-27 17:23:38 +0200

    Make magento module

---
+++ Block/Mimir.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace CognitionHub\Mimir\Block;
+
+class Mimir extends \Magento\Framework\View\Element\Template
+{
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context
+
+    ) {
+        parent::__construct($context, []);
+    }
+
+}
---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'CognitionHub_Mimir',
+    __DIR__
+);
