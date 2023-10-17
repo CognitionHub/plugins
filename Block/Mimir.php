acb8f80a42666432da73cfc8c35bd44743538763
Jens <jensakrr@gmail.com>
2023-10-17 09:55:45 +0200

    Fix user id cache management

--- Block/Mimir.php
+++ Block/Mimir.php
@@ -3,29 +3,7 @@
 namespace CognitionHub\Mimir\Block;
 
 use \Magento\Framework\View\Element\Template\Context;
-use Magento\Customer\Model\Session;
-use Magento\Framework\Stdlib\CookieManagerInterface;
 
 class Mimir extends \Magento\Framework\View\Element\Template
 {
-    protected $customerSession;
-    protected $cookieManager;
-
-    public function __construct(
-        Context $context,
-        Session $customerSession,
-        CookieManagerInterface $cookieManager
-    ) {
-        $this->customerSession = $customerSession;
-        $this->cookieManager = $cookieManager;
-        parent::__construct($context, []);
-    }
-
-    public function getCustomerId()
-    {
-        if ($this->customerSession->isLoggedIn()) {
-            return $this->customerSession->getCustomer()->getId();
-        }
-        return $this->cookieManager->getCookie('PHPSESSID');;
-    }
 }
